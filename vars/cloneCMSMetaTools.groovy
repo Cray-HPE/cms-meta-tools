bfe58d6829b2fbc5fbe86ea1525c5324b4e4ec43
Mitch Harding <mitchell.harding@hpe.com>
2021-10-27 20:12:48 -0400

    test

--- vars/cloneCMSMetaTools.groovy
+++ vars/cloneCMSMetaTools.groovy
@@ -22,6 +22,6 @@
 
 def call() {
     withCredentials([gitUsernamePassword(credentialsId: 'jenkins-algol60-cray-hpe-github-integration')]) {
-        sh 'git clone --depth 1 --no-single-branch https://github.com/Cray-HPE/cms-meta-tools.git ./cms_meta_tools'
+        sh 'git clone --depth 1 --no-single-branch https://github.com/Cray-HPE/cms-meta-tools.git ./cms_meta_tools && git -C ./cms_meta_tools checkout CASMCMS-7662-test'
     }
 }
